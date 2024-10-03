b20957442d232f507207c791bef7deac9f865173
Vyacheslav Lukin <dwek18@mail.ru>
2024-10-04 00:29:11 +0300

    add Proxy on NGINX

---
+++ src/main/java/ru/netology/User.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class User {
+    @NotBlank(message = "Username cannot be empty")
+    private String user;
+
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
+
+    // Getters and setters
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
